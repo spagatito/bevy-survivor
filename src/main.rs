ccd5f86371ee88d4d3d585b4300459c0fe763e7d
spagatito <spagatito@gmail.com>
2024-03-12 19:59:08 -0400

    feat: adds the camera

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,13 @@
 use bevy::prelude::*;
 
 fn main() {
-    App::new().add_plugins(DefaultPlugins).run();
+    App::new()
+        .add_plugins(DefaultPlugins)
+        .add_systems(Startup, spawn_camera)
+        .insert_resource(ClearColor(Color::rgb(0.2, 0.0, 0.15)))
+        .run();
+}
+
+fn spawn_camera(mut commands: Commands) {
+    commands.spawn(Camera2dBundle::default());
 }
