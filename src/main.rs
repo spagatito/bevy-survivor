d828c6ca13b268619f51c5328640b73a03d5b9fe
spagatito <spagatito@gmail.com>
2024-03-12 22:56:00 -0400

    feat: adds player spawn

--- src/main.rs
+++ src/main.rs
@@ -3,11 +3,22 @@ use bevy::prelude::*;
 fn main() {
     App::new()
         .add_plugins(DefaultPlugins)
-        .add_systems(Startup, spawn_camera)
+        .add_systems(Startup, (spawn_camera, spawn_player).chain())
         .insert_resource(ClearColor(Color::rgb(0.2, 0.0, 0.15)))
         .run();
 }
 
 fn spawn_camera(mut commands: Commands) {
     commands.spawn(Camera2dBundle::default());
+}
+
+
+#[derive(Component)]
+pub struct PlayerTag;
+
+pub fn spawn_player(mut commands: Commands) {
+    commands.spawn((
+        PlayerTag,
+        Name::new("Player"),
+    ));
 }
